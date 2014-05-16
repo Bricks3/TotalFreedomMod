d6f7d55a381553494efd510d21941dfe3daa8e7b
Brandon <admin@awesomefreedom.tk>
2014-05-16 16:42:24 +0100

    Update TFM_PlayerRank.java

--- src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
@@ -76,7 +76,7 @@ public enum TFM_PlayerRank
 
         if (entry != null && entry.isActivated())
         {
-            if (sender.getName().equals("markbyron"))
+            if (sender.getName().equals("TheFlameGun"))
             {
                 return OWNER;
             }
